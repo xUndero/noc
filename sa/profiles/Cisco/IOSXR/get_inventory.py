06de880315cd73c6f7885f3c0637215194684873
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2019-02-08 16:32:29 +0200

    Fix copy/paste error

--- sa/profiles/Cisco/IOSXR/get_inventory.py
+++ sa/profiles/Cisco/IOSXR/get_inventory.py
@@ -88,7 +88,7 @@ class Script(BaseScript):
         elif "Power Module" in descr or "Power Supply" in descr:
             numbers = name.split()[1].split("/")
             if len(numbers) == 4:  # 0/PS0/M1/SP
-                number = numbers[2][2:]
+                number = numbers[2][1:]
             else:  # 0/PM0/SP
                 number = numbers[1][2:]
             return "PWR", number, pid
