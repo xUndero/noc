ff52e5e4beffccbee04febcf581c04f2f7223982
smile <sysfar@gmail.com>
2019-02-11 14:42:48 +0300

    Fix Alstec.MSPU get_interface_status_ex

--- sa/profiles/Alstec/MSPU/get_interface_status_ex.py
+++ sa/profiles/Alstec/MSPU/get_interface_status_ex.py
@@ -2,10 +2,12 @@
 # ---------------------------------------------------------------------
 # Alstec.MSPU.get_interface_status_ex
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+import six
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetinterfacestatusex import IGetInterfaceStatusEx
@@ -17,6 +19,7 @@ class Script(BaseScript):
     name = "Alstec.MSPU.get_interface_status_ex"
     interface = IGetInterfaceStatusEx
     requires = []
+    HIGH_SPEED = 4294967295
     MAX_REPETITIONS = 40
     MAX_GETNEXT_RETIRES = 0
 
@@ -26,40 +29,66 @@ class Script(BaseScript):
     def get_getnext_retires(self):
         return self.MAX_GETNEXT_RETIRES
 
-    def get_iftable(self, oid):
+    def get_snmp_ifstatus_get_timeout(self):
+        """
+        Timeout for snmp GET request
+        :return:
+        """
+        return self.profile.snmp_ifstatus_get_timeout
+
+    def get_snmp_ifstatus_get_chunk(self):
+        """
+        Aggregate up to *snmp_metrics_get_chunk* oids
+        to one SNMP GET request
+        :return:
+        """
+        return self.profile.snmp_ifstatus_get_chunk
+
+    def get_iftable(self, oid, ifindex=None):
+        """
+        If ifindex - collect information on the given interfaces
+        Else - collect information for all interfaces
+        :return:
+        """
         if "::" in oid:
             oid = mib[oid]
-        for oid, v in self.snmp.getnext(oid,
-                                        max_repetitions=self.get_max_repetitions(),
-                                        max_retries=self.get_getnext_retires()):
-            yield int(oid.rsplit(".", 1)[-1]), v
+        if ifindex:
+            results = self.snmp.get_chunked(
+                oids=["%s.%s" % (oid, i) for i in ifindex],
+                chunk_size=self.get_snmp_ifstatus_get_chunk(),
+                timeout_limits=self.get_snmp_ifstatus_get_timeout()
+            )
+            for k, v in six.iteritems(results):
+                yield int(k.split(".")[-1]), v
+        else:
+            for oid, v in self.snmp.getnext(oid, max_repetitions=self.get_max_repetitions(),
+                                            max_retries=self.get_getnext_retires()):
+                yield int(oid.rsplit(".", 1)[-1]), v
 
     def apply_table(self, r, mib, name, f=None):
-        if not f:
-            def f(x):
-                return x
-        for ifindex, v in self.get_iftable(mib):
+        f = f or (lambda x: x)
+        for ifindex, v in self.get_iftable(mib, list(r)):
             s = r.get(ifindex)
             if s:
                 s[name] = f(v)
 
-    def get_data(self):
+    def get_data(self, interfaces=None):
         # ifIndex -> ifName mapping
         r = {}  # ifindex -> data
         unknown_interfaces = []
-        for ifindex, name in self.get_iftable("IF-MIB::ifName"):
-            try:
-                v = self.profile.convert_interface_name(name)
-            except InterfaceTypeError as e:
-                self.logger.debug(
-                    "Ignoring unknown interface %s: %s",
-                    name, e
-                )
-                unknown_interfaces += [name]
-                continue
-            r[ifindex] = {
-                "interface": v
-            }
+        if interfaces:
+            for i in interfaces:
+                r[i["ifindex"]] = {"interface": i["interface"]}
+        else:
+            for ifindex, name in self.get_iftable("IF-MIB::ifName"):
+                try:
+                    v = self.profile.convert_interface_name(name)
+                except InterfaceTypeError as e:
+                    self.logger.debug("Ignoring unknown interface %s: %s", name, e)
+                    unknown_interfaces += [name]
+                    continue
+                r[ifindex] = {"interface": v}
+        if_index = list(r)
         # Apply ifAdminStatus
         self.apply_table(r, "IF-MIB::ifAdminStatus", "admin_status", lambda x: x == 1)
         # Apply ifOperStatus
@@ -67,7 +96,7 @@ class Script(BaseScript):
         # Apply dot3StatsDuplexStatus
         self.apply_table(r, "EtherLike-MIB::dot3StatsDuplexStatus", "full_duplex", lambda x: x != 2)
         # Apply ifSpeed
-        for ifindex, s in self.get_iftable("IF-MIB::ifSpeed"):
+        for ifindex, s in self.get_iftable("IF-MIB::ifSpeed", if_index):
             ri = r.get(ifindex)
             if ri and "uplink" in ri["interface"]:
                 s = int(s)
@@ -84,5 +113,6 @@ class Script(BaseScript):
 
         return r.values()
 
-    def execute_snmp(self):
-        return self.get_data()
+    def execute_snmp(self, interfaces=None, **kwargs):
+        r = self.get_data(interfaces=interfaces)
+        return r
