fe8a1a968bfeb10446be46fc404a22c86b77befb
smile <sysfar@gmail.com>
2019-02-11 11:17:25 +0300

    Fix Raisecom.RCIOS get_interface_status_ex

--- sa/profiles/Raisecom/RCIOS/get_interface_status_ex.py
+++ sa/profiles/Raisecom/RCIOS/get_interface_status_ex.py
@@ -2,10 +2,12 @@
 # ---------------------------------------------------------------------
 # Raisecom.RCIOS.get_interface_status_ex
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+import six
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetinterfacestatusex import IGetInterfaceStatusEx
@@ -17,79 +19,116 @@ class Script(BaseScript):
     name = "Raisecom.RCIOS.get_interface_status_ex"
     interface = IGetInterfaceStatusEx
     requires = []
+    HIGH_SPEED = 4294967295
+    MAX_REPETITIONS = 10
+    MAX_GETNEXT_RETIRES = 0
 
-    def get_iftable(self, oid):
+    def get_max_repetitions(self):
+        return self.MAX_REPETITIONS
+
+    def get_getnext_retires(self):
+        return self.MAX_GETNEXT_RETIRES
+
+    def get_snmp_ifstatus_get_timeout(self):
+        """
+        Timeout for snmp GET request
+        :return:
+        """
+        return self.profile.snmp_ifstatus_get_timeout
+
+    def get_snmp_ifstatus_get_chunk(self):
+        """
+        Aggregate up to *snmp_metrics_get_chunk* oids
+        to one SNMP GET request
+        :return:
+        """
+        return self.profile.snmp_ifstatus_get_chunk
+
+    def get_iftable(self, oid, ifindex=None):
+        """
+        If ifindex - collect information on the given interfaces
+        Else - collect information for all interfaces
+        :return:
+        """
         if "::" in oid:
             oid = mib[oid]
-        r = {}
-        for oid, v in self.snmp.getnext(oid, max_repetitions=40):
-            ifindex = int(oid.split(".")[-1])
-            r[ifindex] = v
-        return r
+        if ifindex:
+            results = self.snmp.get_chunked(
+                oids=["%s.%s" % (oid, i) for i in ifindex],
+                chunk_size=self.get_snmp_ifstatus_get_chunk(),
+                timeout_limits=self.get_snmp_ifstatus_get_timeout()
+            )
+            for k, v in six.iteritems(results):
+                yield int(k.split(".")[-1]), v
+        else:
+            for oid, v in self.snmp.getnext(oid, max_repetitions=self.get_max_repetitions(),
+                                            max_retries=self.get_getnext_retires()):
+                yield int(oid.rsplit(".", 1)[-1]), v
 
     def apply_table(self, r, mib, name, f=None):
-        if not f:
-            f = lambda x: x
-        d = self.get_iftable(mib)
-        for ifindex in d:
-            if ifindex in r:
-                r[ifindex][name] = f(d[ifindex])
+        f = f or (lambda x: x)
+        for ifindex, v in self.get_iftable(mib, list(r)):
+            s = r.get(ifindex)
+            if s:
+                s[name] = f(v)
 
-    def get_data(self):
+    def get_data(self, interfaces=None):
         # ifIndex -> ifName mapping
         r = {}  # ifindex -> data
         unknown_interfaces = []
-        for oid, name in self.snmp.getnext(mib["IF-MIB::ifDescr"]):
-            try:
-                name = name.split()[1].strip()
-                v = self.profile.convert_interface_name(name)
-            except InterfaceTypeError, why:
-                self.logger.debug(
-                    "Ignoring unknown interface %s: %s",
-                    name, why
-                )
-                unknown_interfaces += [name]
-                continue
-            ifindex =  int(oid.split(".")[-1])
-            r[ifindex] = {
-                "interface": v
-            }
+        if interfaces:
+            for i in interfaces:
+                r[i["ifindex"]] = {"interface": i["interface"]}
+        else:
+            for ifindex, name in self.get_iftable("IF-MIB::ifDescr"):
+                try:
+                    name = name.split()[1].strip()
+                    v = self.profile.convert_interface_name(name)
+                except InterfaceTypeError as e:
+                    self.logger.debug("Ignoring unknown interface %s: %s", name, e)
+                    unknown_interfaces += [name]
+                    continue
+                r[ifindex] = {"interface": v}
+        if_index = list(r)
         # Apply ifAdminStatus
         self.apply_table(r, "IF-MIB::ifAdminStatus", "admin_status", lambda x: x == 1)
         # Apply ifOperStatus
         self.apply_table(r, "IF-MIB::ifOperStatus", "oper_status", lambda x: x == 1)
+        # Apply dot3StatsDuplexStatus
+        self.apply_table(r, "EtherLike-MIB::dot3StatsDuplexStatus", "full_duplex", lambda x: x != 2)
         # Apply ifSpeed
-        s_table = self.get_iftable("IF-MIB::ifSpeed")
         highspeed = set()
-        for ifindex in r:
-            s = s_table.get(ifindex)
-            if s is not None:
-                s = int(s)
-                if s == 4294967295:
+        for ifindex, s in self.get_iftable("IF-MIB::ifSpeed", if_index):
+            ri = r.get(ifindex)
+            if ri and s is not None:
+                # s is None if OID is not exists
+                if self.is_high_speed(ri, s):
                     highspeed.add(ifindex)
                 elif s:
                     r[ifindex]["in_speed"] = s // 1000
                     r[ifindex]["out_speed"] = s // 1000
         # Refer to ifHighSpeed if necessary
         if highspeed:
-            hs_table = self.get_iftable("IF-MIB::ifHighSpeed")
-            for ifindex in highspeed:
-                s = hs_table.get(ifindex)
-                if s is not None:
+            for ifindex, s in self.get_iftable("IF-MIB::ifHighSpeed", if_index):
+                if ifindex in highspeed and s is not None:  # s is None if OID is not exists
                     s = int(s)
                     if s:
                         r[ifindex]["in_speed"] = s * 1000
                         r[ifindex]["out_speed"] = s * 1000
+        # Log unknown interfaces
         if unknown_interfaces:
-            self.logger.info("%d unknown interfaces has been ignored",
-                             len(unknown_interfaces))
+            self.logger.info("%d unknown interfaces has been ignored", len(unknown_interfaces))
         return r.values()
 
-    def execute(self):
-        r = []
-        if self.has_snmp():
-            try:
-                r= self.get_data()
-            except self.snmp.TimeOutError:
-                pass
+    def is_high_speed(self, data, speed):
+        """
+        Detect should we check ifHighSpeed
+        :param data: dict with
+        :param speed:
+        :return:
+        """
+        return speed == self.HIGH_SPEED
+
+    def execute_snmp(self, interfaces=None, **kwargs):
+        r = self.get_data(interfaces=interfaces)
         return r
