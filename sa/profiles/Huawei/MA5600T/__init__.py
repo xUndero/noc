e6e63460684e89e99166cc72eb2845429bed8de4
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2019-02-12 17:53:54 +0200

    Fix

--- sa/profiles/Huawei/MA5600T/__init__.py
+++ sa/profiles/Huawei/MA5600T/__init__.py
@@ -6,8 +6,8 @@
 # Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-
-
+"""
+"""
 from noc.core.profile.base import BaseProfile
 import re
 
