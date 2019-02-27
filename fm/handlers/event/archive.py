546dcb45e52a2f56df1d9308614958dc2794cddb
smile <sysfar@gmail.com>
2019-02-27 14:26:23 +0300

    Fix Generic get_interface_status_ex

---
+++ fm/handlers/event/archive.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+#  Mark Archive Event when close
+#  Rules handlers
+# ----------------------------------------------------------------------
+#  Copyright (C) 2007-2019 The NOC Project
+#  See LICENSE for details
+# ----------------------------------------------------------------------
+
+def handler(event):
+    event.mark_as_archived("Close event")
