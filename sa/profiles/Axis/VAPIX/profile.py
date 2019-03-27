98eadcb6b2793604e7ab1e38b39508255d17eb65
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2019-03-27 16:39:56 +0200

    Add get_local_users script

--- sa/profiles/Axis/VAPIX/profile.py
+++ sa/profiles/Axis/VAPIX/profile.py
@@ -13,17 +13,20 @@ from noc.core.profile.base import BaseProfile
 class Profile(BaseProfile):
     name = "Axis.VAPIX"
 
-    def get_list(self, script):
-        v = script.http.get(
-            "/axis-cgi/admin/param.cgi?action=list",
-            eof_mark="root.Time.NTP", cached=True, use_basic=True
-        )
+    def get_list(self, script, command=None, eof_mark=None):
+        if command is None:
+            command = "/param.cgi?action=list"
+            eof_mark = "root.Time.NTP"
+        command = "/axis-cgi/admin" + command
+        v = script.http.get(command, eof_mark=eof_mark, cached=True, use_basic=True)
         return v
 
-    def get_dict(self, script):
+    def get_dict(self, script, command=None, eof_mark=None):
         r = {}
-        v = self.get_list(script)
+        v = self.get_list(script, command, eof_mark)
         for line in v.splitlines():
             key, value = line.split("=", 1)
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1]
             r[key] = value
         return r
