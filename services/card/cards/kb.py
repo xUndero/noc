d7260bc5916ed8fae020f2de8adea74dbee25942
Andrey Vertiprahov <aversanta@gmail.com>
2019-04-24 15:23:42 +0500

    Add Knowledge Base card.

---
+++ services/card/cards/kb.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# KB card
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+from __future__ import absolute_import
+# NOC modules
+from noc.kb.models.kbentry import KBEntry
+from .base import BaseCard
+
+
+class KBCard(BaseCard):
+    name = "kb"
+    default_template_name = "kbentry"
+    model = KBEntry
+
+    def get_object(self, id):
+        if id == "0":
+            return KBEntry
+        return super(KBCard, self).get_object(id)
+
+    def get_template_name(self):
+        if self.id == "0":
+            return "kb"
+        return self.default_template_name
+
+    def get_data(self):
+        return {
+            "object": self.object
+        }
