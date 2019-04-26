00b90473d022a1562fb96784cc33bdf783bcca9a
bee26 <bee26@ya.ru>
2019-04-26 06:19:05 +0100

    Update get_capabilities.py

--- sa/profiles/Qtech/QSW2800/get_capabilities.py
+++ sa/profiles/Qtech/QSW2800/get_capabilities.py
@@ -95,6 +95,8 @@ class Script(BaseScript):
         if s:
             caps["Stack | Members"] = len(s) if len(s) != 1 else 0
             caps["Stack | Member Ids"] = " | ".join(s)
+        for m in self.has_snmp_memory():
+            caps[m] = True
 
     def execute_platform_snmp(self, caps):
         for m in self.has_snmp_memory():
