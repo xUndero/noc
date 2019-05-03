e8119a01fd0fe5ebba415923ce03be1074237690
Andrey Vertiprahov <aversanta@gmail.com>
2019-05-03 17:51:22 +0500

    Fix sa vendor migration for old installation.

--- sa/migrations/0151_managed_object_vendor.py
+++ sa/migrations/0151_managed_object_vendor.py
@@ -13,6 +13,24 @@ from south.db import db
 from noc.core.model.fields import DocumentReferenceField
 from noc.lib.nosql import get_db
 
+OLD_VENDOR_MAP = {
+    "Alcatel-Lucent": "ALU",
+    "Arista Networks": "ARISTA",
+    "Edge-Core": "EDGECORE",
+    "Cisco Networks": "CISCO",
+    "D-Link": "DLINK",
+    "Extreme Networks": "EXTREME",
+    "f5 Networks": "F5",
+    "Force10 Networks": "FORCE10",
+    "Huawei Technologies Co.": "HUAWEI",
+    "HP": "HP",
+    "Juniper Networks": "JUNIPER",
+    "NOC": "NOC",
+    "NoName": "NONAME",
+    "ZTE": "ZTE",
+    "ZyXEL": "ZYXEL"
+}
+
 
 class Migration(object):
     def forwards(self):
@@ -25,12 +43,19 @@ class Migration(object):
             "SELECT DISTINCT value FROM sa_managedobjectattribute WHERE key = 'vendor'"))
         # Create vendors records
         pcoll = get_db()["noc.vendors"]
-        inventory_vendors = {v["name"]: v["_id"] for v in pcoll.find()}
+        inventory_vendors = {}
+        for v in pcoll.find():
+            if "code" in v:
+                inventory_vendors[v["code"][0] if isinstance(v["code"], list) else v["code"]] = v["_id"]
+            elif v["name"] in OLD_VENDOR_MAP:
+                inventory_vendors[OLD_VENDOR_MAP[v["name"]]] = v["_id"]
+            else:
+                inventory_vendors[v["name"].split(" ")[0].upper()] = v["_id"]
         for v in vendors.union(set(inventory_vendors)):
             u = uuid.uuid4()
             vc = v.upper()
             if v in inventory_vendors:
-                pcoll.update({
+                pcoll.update_one({
                     "_id": inventory_vendors[v]
                 }, {
                     "$set": {
@@ -39,7 +64,7 @@ class Migration(object):
                     }
                 })
             else:
-                pcoll.update({
+                pcoll.update_one({
                     "code": vc
                 }, {
                     "$set": {
