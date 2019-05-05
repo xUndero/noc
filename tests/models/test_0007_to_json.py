28dcf48e716a374406c7e32490f0d1507f1f68c5
Dmitry Volodin <dv@nocproject.org>
2019-05-05 09:16:15 +0300

    Fix Layer.to_json()

--- tests/models/test_0007_to_json.py
+++ tests/models/test_0007_to_json.py
@@ -10,13 +10,17 @@
 from __future__ import absolute_import
 # Third-party modules
 import pytest
+import six
 # NOC modules
 from .util import get_documents
 
 
-@pytest.mark.parametrize("model", [x for x in get_documents() if hasattr(x, "to_json")])
+@pytest.mark.parametrize("model", [
+    x for x in get_documents()
+    if hasattr(x, "to_json") and hasattr(x, "get_json_path")
+])
 def test_document_to_json(model):
     for o in model.objects.all():
         j = o.to_json()
         assert j
-        assert isinstance(j, dict)
+        assert isinstance(j, six.string_types)
