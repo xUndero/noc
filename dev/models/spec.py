f7a18f6349358ac656021a26f2f867d30cc305bb
kk <kk@buhaha.net>
2019-05-06 22:49:51 +0300

    Model __str__ methods. dev

--- dev/models/spec.py
+++ dev/models/spec.py
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------
 # Spec model
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
@@ -12,6 +12,7 @@ import os
 import operator
 from threading import Lock
 # Third-party modules
+import six
 from mongoengine.document import Document, EmbeddedDocument
 from mongoengine.fields import (StringField, EmbeddedDocumentField, DateTimeField,
                                 ListField, UUIDField, IntField)
@@ -26,6 +27,7 @@ from .quiz import Quiz, Q_TYPES
 id_lock = Lock()
 
 
+@six.python_2_unicode_compatible
 class SpecChange(EmbeddedDocument):
     date = DateTimeField()
     changes = StringField()
@@ -76,7 +78,7 @@ class Spec(Document):
     _id_cache = cachetools.TTLCache(maxsize=100, ttl=60)
     _name_cache = cachetools.TTLCache(maxsize=100, ttl=60)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
     @classmethod
