2ba393eda431b560c17ed50d9c0af66a79c2b133
Dmitry Volodin <dv@nocproject.org>
2019-05-07 17:08:05 +0300

    Replace pylibssh2 with ssh2-python

--- core/script/cli/ssh.py
+++ core/script/cli/ssh.py
@@ -2,37 +2,65 @@
 # ----------------------------------------------------------------------
 # SSH CLI
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
 # Python modules
 from __future__ import absolute_import
 import os
+import threading
+import operator
+import logging
 
 # Third-party modules modules
-import six
+import cachetools
 from tornado.iostream import IOStream
 import tornado.gen
-import libssh2
-import _libssh2
+from ssh2.session import Session, LIBSSH2_HOSTKEY_HASH_SHA1
+from ssh2.exceptions import SSH2Error
+from ssh2.error_codes import LIBSSH2_ERROR_EAGAIN
 
 # NOC modules
+from noc.config import config
 from .base import CLI
 from .error import CLIAuthFailed, CLISSHProtocolError
-from noc.config import config
+
+key_lock = threading.Lock()
+logger = logging.getLogger(__name__)
 
 
 class SSHIOStream(IOStream):
     SSH_KEY_PREFIX = config.path.ssh_key_prefix
+
+    _key_cache = cachetools.TTLCache(100, ttl=60)
 
     def __init__(self, sock, cli, *args, **kwargs):
         super(SSHIOStream, self).__init__(sock, *args, **kwargs)
         self.cli = cli
         self.script = self.cli.script
         self.logger = cli.logger
-        self.session = libssh2.Session()
+        self.session = None
         self.channel = None
+
+    @classmethod
+    @cachetools.cachedmethod(operator.attrgetter("_key_cache"), lock=lambda _: key_lock)
+    def get_publickey(cls, pool):
+        """
+        Return public, private key pair
+        :return: bytes, bytes or None, None
+        """
+        logger.debug("Getting keys for pool %s", pool)
+        pub_path = os.path.join(config.path.ssh_key_prefix, pool, "id_rsa.pub")
+        priv_path = os.path.join(config.path.ssh_key_prefix, pool, "id_rsa")
+        if not os.path.exists(pub_path):
+            logger.debug("No public key for pool %s (%s)", pool, pub_path)
+            return None, None
+        if not os.path.exists(priv_path):
+            logger.debug("No private key for pool %s (%s)", pool, priv_path)
+            return None, None
+        with open(pub_path) as fpub, open(priv_path) as fpriv:
+            return fpub.read(), fpriv.read()
 
     @tornado.gen.coroutine
     def startup(self):
@@ -43,11 +71,12 @@ class SSHIOStream(IOStream):
         if user is None:
             user = ""
         self.logger.debug("Startup ssh session for user '%s'", user)
+        self.session = Session()
         try:
-            self.session.startup(self.socket)
-            host_hash = self.session.hostkey_hash(2)  # SHA1
+            self.session.handshake(self.socket)
+            host_hash = self.session.hostkey_hash(LIBSSH2_HOSTKEY_HASH_SHA1)
             self.logger.debug("Connected. Host fingerprint is %s", host_hash.encode("hex"))
-            auth_methods = self.session.userauth_list(user).split(",")
+            auth_methods = self.session.userauth_list(user)
             self.logger.debug("Supported authentication methods: %s", ", ".join(auth_methods))
             # Try to authenticate
             authenticated = False
@@ -57,61 +86,57 @@ class SSHIOStream(IOStream):
                     authenticated |= ah()
                     if authenticated:
                         break
-            if authenticated:
-                self.logger.debug("User is authenticated")
-            else:
+            if not authenticated:
                 self.logger.error("Failed to authenticate user '%s'", user)
                 raise CLIAuthFailed("Failed to log in")
+            self.logger.debug("User is authenticated")
             self.logger.debug("Open channel")
             self.channel = self.session.open_session()
             self.channel.pty("xterm")
             self.channel.shell()
-            self.channel.setblocking(0)
-        except _libssh2.Error as e:
+            self.session.keepalive_config(False, 60)
+            self.session.set_blocking(False)
+        except SSH2Error as e:
+            self.logger.info("SSH Error: %s", e)
             raise CLISSHProtocolError("SSH Error: %s" % e)
 
     def read_from_fd(self):
         try:
-            r = self.channel.read(self.read_chunk_size)
-            if r is None:
+            code, data = self.channel.read(self.read_chunk_size)
+            if code == 0:
                 if self.channel.eof():
                     self.logger.info("SSH session reset")
                     self.close()
                 return None
-            elif r == -37:  # LIBSSH2_ERROR_EAGAIN
-                return None  # blocking call
-            elif isinstance(r, six.string_types):
-                return r
-            else:
-                raise CLISSHProtocolError("SSH Error code %s" % r)
-        except _libssh2.Error as e:
+            elif code > 0:
+                return data
+            elif code == LIBSSH2_ERROR_EAGAIN:
+                return None  # Blocking call
+            raise CLISSHProtocolError("SSH Error code %s" % code)
+        except SSH2Error as e:
             raise CLISSHProtocolError("SSH Error: %s" % e)
 
     def write_to_fd(self, data):
-        # libssh2 doesn't accept memoryview
+        # ssh2 doesn't accept memoryview
         if isinstance(data, memoryview):
             data = data.tobytes()
         try:
-            return self.channel.write(data)
-        except _libssh2.Error as e:
+            _, written = self.channel.write(data)
+            return written
+        except SSH2Error as e:
             raise CLISSHProtocolError("SSH Error: %s" % e)
 
     def close(self, exc_info=False):
         if not self.closed():
             if self.channel:
-                self.channel.setblocking(1)
                 self.logger.debug("Closing channel")
                 try:
                     self.channel.close()
-                except _libssh2.Error as e:
+                except SSH2Error as e:
                     self.logger.debug("Cannot close channel clearly: %s", e)
                 self.channel = None
             if self.session:
                 self.logger.debug("Closing ssh session")
-                try:
-                    self.session.close()
-                except _libssh2.Error as e:
-                    self.logger.debug("Cannot close session clearly: %s", e)
                 self.session = None
         super(SSHIOStream, self).close(exc_info=exc_info)
 
@@ -120,20 +145,19 @@ class SSHIOStream(IOStream):
         Public key authentication
         """
         self.logger.debug("Trying publickey authentication")
-        pub_path = os.path.join(self.SSH_KEY_PREFIX, self.script.pool, "id_rsa.pub")
-        priv_path = os.path.join(self.SSH_KEY_PREFIX, self.script.pool, "id_rsa")
-        self.logger.debug("public_key=%s private_key=%s", pub_path, priv_path)
+        pub_key, priv_key = self.get_publickey(self.script.pool)
+        if not pub_key or not priv_key:
+            self.logger.debug("No keys for pool. Skipping")
+            return False
         user = self.script.credentials["user"]
         if user is None:
             user = ""
         try:
-            self.session.userauth_publickey_fromfile(
-                user, publickey=pub_path, privatekey=priv_path, passphrase=""
-            )
+            self.session.userauth_publickey_frommemory(user, priv_key, "", pub_key)
             return True
-        except _libssh2.Error:
-            code, msg = self.session.last_error()
-            self.logger.debug("Failed: %s (Code: %s)", msg, code)
+        except SSH2Error as e:
+            msg = self.session.last_error()
+            self.logger.debug("Failed: %s", msg)
             return False
 
     def auth_keyboardinteractive(self):
@@ -141,6 +165,9 @@ class SSHIOStream(IOStream):
         Keyboard-interactive authentication. Send username and password
         """
         self.logger.debug("Trying keyboard-interactive")
+        if not hasattr(self.session, "userauth_keyboardinteractive"):
+            self.logger.debug("keyboard-interactive is not supported by ssh library. Skipping")
+            return False
         user = self.script.credentials["user"]
         if user is None:
             user = ""
@@ -151,10 +178,9 @@ class SSHIOStream(IOStream):
             self.session.userauth_keyboardinteractive(user, password)
             self.logger.debug("Success")
             return True
-        except _libssh2.Error:
-            code, msg = self.session.last_error()
-
-            self.logger.debug("Failed: %s (Code: %s)", msg, code)
+        except SSH2Error:
+            msg = self.session.last_error()
+            self.logger.debug("Failed: %s", msg)
             return False
 
     def auth_password(self):
@@ -172,10 +198,9 @@ class SSHIOStream(IOStream):
             self.session.userauth_password(user, password)
             self.logger.debug("Success")
             return True
-        except _libssh2.Error:
-            code, msg = self.session.last_error()
-
-            self.logger.debug("Failed: %s (Code: %s)", msg, code)
+        except SSH2Error:
+            msg = self.session.last_error()
+            self.logger.debug("Failed: %s", msg)
             return False
 
 
