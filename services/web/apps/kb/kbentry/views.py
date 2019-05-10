cd1c376dfab86666f31a390bea968233b3ffa354
Andrey Vertiprahov <aversanta@gmail.com>
2019-05-10 19:35:36 +0500

    Add file upload backend.

--- services/web/apps/kb/kbentry/views.py
+++ services/web/apps/kb/kbentry/views.py
@@ -6,6 +6,9 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+import re
+# Third-party modules
 from django.http import HttpResponse
 import mimetypes
 # NOC modules
@@ -23,10 +26,13 @@ class KBEntryApplication(ExtModelApplication):
     title = _("Entries")
     menu = [_("Setup"), _("Entries")]
     model = KBEntry
+    file_fields_mask = re.compile(r"^(?P<fname>description|is_hidden)(?P<findex>\d+)")
 
     def instance_to_dict(self, o, fields=None):
         r = super(KBEntryApplication, self).instance_to_dict(o, fields=fields)
-        # del r["body"]
+        r["attachments"] = [{
+            "name": x.name, "size": x.size, "mtime": self.to_json(x.mtime), "description": x.description}
+            for x in KBEntryAttachment.objects.filter(kb_entry=o, is_hidden=False).order_by("name")]
         return r
 
     @view(r"^(?P<id>\d+)/history/$", access="read", api=True)
@@ -45,7 +51,7 @@ class KBEntryApplication(ExtModelApplication):
         return KBEntry.most_popular()
 
     @view(r"^(?P<id>\d+)/attachments/$", access="read", api=True, method=["GET"])
-    def api_post_set_attachments(self, request, id):
+    def api_list_attachments(self, request, id):
         o = self.get_object_or_404(KBEntry, id=id)
         return [{"name": x.name, "size": x.size, "mtime": self.to_json(x.mtime), "description": x.description}
                 for x in KBEntryAttachment.objects.filter(kb_entry=o, is_hidden=False).order_by("name")]
@@ -59,7 +65,7 @@ class KBEntryApplication(ExtModelApplication):
         response["Content-Disposition"] = "attachment; filename=\"%s\"" % attach.file.name
         return response
 
-    @view(r"^(?P<id>\d+)/attachment/(?P<name>.+)/$", access="read", api=True, method=["DELETE"])
+    @view(r"^(?P<id>\d+)/attachment/(?P<name>.+)/$", access="delete", api=True, method=["DELETE"])
     def api_delete_attachment(self, request, id, name):
         o = self.get_object_or_404(KBEntry, id=id)
         attach = self.get_object_or_404(KBEntryAttachment, kb_entry=o, name=name)
@@ -72,3 +78,24 @@ class KBEntryApplication(ExtModelApplication):
         attach = KBEntryAttachment(kb_entry=o, name="uploaded_file1", description="", file=request.FILES["file"])
         attach.save()
         return self.response({"result": "Upload succesful"}, status=self.OK)
+
+    def update_file(self, files, o, attrs=None):
+        left = {}  # name -> data
+        for f in files:
+            left[f] = files[f]
+        # errors = {}
+        # failed = False
+        for name in left:
+            f = left[name]
+            attr = attrs.get(name[4:], {})
+            attach = KBEntryAttachment.objects.filter(kb_entry=o, name=name)
+            # @todo update attributes
+            if not attach:
+                attach = KBEntryAttachment(kb_entry=o, name=f.name, description=attr.get("description", ""),
+                                           is_hidden=attr.get("is_hidden") == "true", file=f)
+                attach.save()
+        return True
+
+    @view(r"^(?P<id>\d+)/?$", access="update", api=True, method=["POST"])
+    def api_post_update(self, request, id):
+        return self.api_update(request, id)
