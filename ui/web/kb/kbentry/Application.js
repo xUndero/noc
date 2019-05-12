49e61b04471914eee9b0adf25a7e75d56f8f2c6b
Andrey Vertiprahov <aversanta@gmail.com>
2019-05-12 11:33:40 +0500

    Fix internal KB link.

--- ui/web/kb/kbentry/Application.js
+++ ui/web/kb/kbentry/Application.js
@@ -175,7 +175,7 @@ Ext.define("NOC.kb.kbentry.Application", {
             }
         });
         Ext.Ajax.request({
-            method: me.currentRecord ? "PUT" : "POST",
+            method: me.currentRecord ? "POST" : "POST",
             url: me.base_url + (me.currentRecord ? me.currentRecord.get([me.idField]) + "/" : ""),
             rawData: data,
             headers: {"Content-Type": null},
