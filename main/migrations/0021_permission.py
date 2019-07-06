0367b8622afe2fe8dfdd85e2d3f0be665d9890d2
Dmitry Volodin <dv@nocproject.org>
2019-07-06 16:02:04 +0300

    black: migrations

--- main/migrations/0021_permission.py
+++ main/migrations/0021_permission.py
@@ -8,11 +8,10 @@
 
 # Third-party modules
 from django.db import models
+
 # NOC modules
 from noc.core.migration.base import BaseMigration
 
 
 class Migration(BaseMigration):
-    depends_on = [
-        ("aaa", "0003_permission")
-    ]
+    depends_on = [("aaa", "0003_permission")]
