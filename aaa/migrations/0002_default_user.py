73372fcfb3a3d42335d93aef99a66be84fe21dd9
Aleksey Shirokih <shirokih@nocproject.org>
2019-07-27 22:31:16 +0300

    Black it

--- aaa/migrations/0002_default_user.py
+++ aaa/migrations/0002_default_user.py
@@ -21,23 +21,11 @@ class Migration(BaseMigration):
         # Create default admin user if no user exists
         if not self.db.execute("SELECT COUNT(*) FROM auth_user")[0][0] == 0:
             return
-        admin_password = os.environ.get(
-            "NOC_DEFAULT_ADMIN_PASSWORD",
-            "admin")
-        admin_email = os.environ.get(
-            "NOC_DEFAULT_ADMIN_EMAIL",
-            "test@example.com")
+        admin_password = os.environ.get("NOC_DEFAULT_ADMIN_PASSWORD", "admin")
+        admin_email = os.environ.get("NOC_DEFAULT_ADMIN_EMAIL", "test@example.com")
         self.db.execute(
             "INSERT INTO auth_user"
             "(username, first_name, last_name, email, password, is_active, is_superuser, date_joined) "
             "VALUES(%s, %s, %s, %s, %s, %s, %s, 'now')",
-            [
-                "admin",
-                "NOC",
-                "Admin",
-                admin_email,
-                make_password(admin_password),
-                True,
-                True,
-            ],
+            ["admin", "NOC", "Admin", admin_email, make_password(admin_password), True, True],
         )
