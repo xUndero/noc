d2a0bea09cd8699e296f9da0e8423cf47a1f2abf
Andrey Vertiprahov <aversanta@gmail.com>
2019-07-30 21:09:28 +0500

    Add confdb support to Beward.BD profile.

--- sa/profiles/Beward/BD/profile.py
+++ sa/profiles/Beward/BD/profile.py
@@ -8,8 +8,16 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python module
+import re
+
+# NOC modules
 from noc.core.profile.base import BaseProfile
 
 
 class Profile(BaseProfile):
     name = "Beward.BD"
+    config_tokenizer = "line"
+    config_tokenizer_settings = {"line_comment": "#",
+                                 "rewrite": [(re.compile(r"[\.=\[\]]"), " ")]}
+    config_normalizer = "BDNormalizer"
