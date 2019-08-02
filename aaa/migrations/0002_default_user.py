b911507e06a30d5d3725659c0ed69edaaa2ba278
Dmitry Volodin <dv@nocproject.org>
2019-08-02 12:06:46 +0300

    `initial` section in config

--- aaa/migrations/0002_default_user.py
+++ aaa/migrations/0002_default_user.py
@@ -6,14 +6,12 @@
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
-# Python modules
-import os
-
 # Third-party modules
 from django.contrib.auth.hashers import make_password
 
 # NOC modules
 from noc.core.migration.base import BaseMigration
+from noc.config import config
 
 
 class Migration(BaseMigration):
@@ -21,11 +19,17 @@ class Migration(BaseMigration):
         # Create default admin user if no user exists
         if not self.db.execute("SELECT COUNT(*) FROM auth_user")[0][0] == 0:
             return
-        admin_password = os.environ.get("NOC_DEFAULT_ADMIN_PASSWORD", "admin")
-        admin_email = os.environ.get("NOC_DEFAULT_ADMIN_EMAIL", "test@example.com")
         self.db.execute(
             "INSERT INTO auth_user"
             "(username, first_name, last_name, email, password, is_active, is_superuser, date_joined) "
             "VALUES(%s, %s, %s, %s, %s, %s, %s, 'now')",
-            ["admin", "NOC", "Admin", admin_email, make_password(admin_password), True, True],
+            [
+                config.initial.admin_user_name,
+                "NOC",
+                "Admin",
+                config.initial.admin_email,
+                make_password(config.initial.admin_password),
+                True,
+                True,
+            ],
         )
