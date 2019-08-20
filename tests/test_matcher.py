f665e00e290e22df235b175b588cbb74b33e24e7
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2019-08-20 10:02:54 +0300

    Fix formatting

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -51,10 +51,10 @@ def test_eq_and(raw, config, expected):
         ),
         (
             {"vendor": "Eltex", "image": "_image.bin"},
-            {"image": {"$regex": "^\S+"}, "vendor": "Eltex"},
+            {"image": {"$regex": r"^\S+"}, "vendor": "Eltex"},
             True,
         ),
-        ({"vendor": "Eltex"}, {"image": {"$regex": "^\S+"}, "vendor": "Eltex"}, False),
+        ({"vendor": "Eltex"}, {"image": {"$regex": r"^\S+"}, "vendor": "Eltex"}, False),
     ],
 )
 def test_regex(raw, config, expected):
