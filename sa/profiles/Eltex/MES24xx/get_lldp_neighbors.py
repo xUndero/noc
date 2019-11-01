1fe0e6cef3e53d044c84d7223955863c0bb4b108
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2019-11-01 17:26:37 +0200

    Fix

--- sa/profiles/Eltex/MES24xx/get_lldp_neighbors.py
+++ sa/profiles/Eltex/MES24xx/get_lldp_neighbors.py
@@ -9,13 +9,18 @@
 # Python modules
 import re
 
+# Third-party modules
+import six
+
 # NOC modules
 from noc.sa.profiles.Generic.get_lldp_neighbors import Script as BaseScript
 from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
+from noc.core.mib import mib
 from noc.core.lldp import (
     LLDP_CHASSIS_SUBTYPE_MAC,
     LLDP_CHASSIS_SUBTYPE_LOCAL,
     LLDP_PORT_SUBTYPE_MAC,
+    LLDP_PORT_SUBTYPE_NAME,
     LLDP_PORT_SUBTYPE_LOCAL,
     LLDP_CAP_OTHER,
     LLDP_CAP_REPEATER,
@@ -48,9 +53,43 @@ class Script(BaseScript):
         re.MULTILINE | re.DOTALL,
     )
 
-    CAPS_MAP = {"R": 2, "B": 4, "r": 16}
     CHASSIS_SUBTYPE = {"Mac Address": LLDP_CHASSIS_SUBTYPE_MAC, "Local": LLDP_CHASSIS_SUBTYPE_LOCAL}
-    PORT_SUBTYPE = {"Mac Address": LLDP_PORT_SUBTYPE_MAC, "Local": LLDP_PORT_SUBTYPE_LOCAL}
+    PORT_SUBTYPE = {
+        "Mac Address": LLDP_PORT_SUBTYPE_MAC,
+        "Interface Name": LLDP_PORT_SUBTYPE_NAME,
+        "Local": LLDP_PORT_SUBTYPE_LOCAL,
+    }
+
+    def get_local_iface(self):
+        r = {}
+        names = {x: y for y, x in six.iteritems(self.scripts.get_ifindexes())}
+        # Get LocalPort Table
+        for port_num, port_subtype, port_id, port_descr in self.snmp.get_tables(
+            [
+                mib["LLDP-MIB::lldpLocPortIdSubtype"],
+                mib["LLDP-MIB::lldpLocPortId"],
+                mib["LLDP-MIB::lldpLocPortDesc"],
+            ]
+        ):
+            if port_subtype == 1:
+                # Iface alias
+                iface_name = port_id  # BUG. Look in PortID instead PortDesc
+            elif port_subtype == 3:
+                # Iface MAC address
+                raise NotImplementedError()
+            elif port_subtype == 7 and port_id.isdigit():
+                # Iface local (ifindex)
+                iface_name = names[int(port_id)]
+            else:
+                # Iface local
+                iface_name = port_id
+            r[port_num] = {"local_interface": iface_name, "local_interface_subtype": port_subtype}
+        if not r:
+            self.logger.warning(
+                "Not getting local LLDP port mappings. Check 1.0.8802.1.1.2.1.3.7 table"
+            )
+            raise NotImplementedError()
+        return r
 
     def execute_cli(self):
         r = []
